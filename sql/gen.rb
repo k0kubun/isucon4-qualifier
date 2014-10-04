75e7255cb75457f1843fac3f7aef0b3304af9e38
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-04 11:57:29 +0900

    Update gen.rb

--- sql/gen.rb
+++ sql/gen.rb
@@ -3,8 +3,8 @@ source = ""
 text = File.read("./dummy_users.tsv")
 text.each_line do |line|
   id, login, pass, salt, password_hash = line.strip.split("\t")
-  source += "#{id} => { id: #{id}, salt: '#{salt}', password_hash: '#{password_hash}' }"
-  break
+  source += ":'#{login}' => { id: #{id}, salt: '#{salt}', password_hash: '#{password_hash}' }"
+  print "#{id}         \r"
 end
 
 File.write("src.rb", source)
