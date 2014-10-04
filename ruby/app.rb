740da8fba68cab92e2066b54ebe598843ea94a8d
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-04 13:45:15 +0900

    Don't use erb

--- ruby/app.rb
+++ ruby/app.rb
@@ -10,7 +10,6 @@ require_relative "./users"
 
 module Isucon4
   class App < Sinatra::Base
-    # use Rack::Session::Cookie, secret: ENV['ISU4_SESSION_SECRET'] || 'shirokane'
     use Rack::Session::Redis, {
       url: "redis://localhost:6379/0",
       namespace: "rack:session",
@@ -174,7 +173,69 @@ module Isucon4
       unless current_user
         redirect '/?err=invalid'
       end
-      erb :mypage, layout: :base
+
+      <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="/stylesheets/bootflat.min.css">
+    <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
+    <title>isucon4</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 id="topbar">
+        <a href="/"><img src="/images/isucon-bank.png" alt="いすこん銀行 オンラインバンキングサービス"></a>
+      </h1>
+      <div class="alert alert-success" role="alert">
+  ログインに成功しました。<br>
+  未読のお知らせが０件、残っています。
+</div>
+
+<dl class="dl-horizontal">
+  <dt>前回ログイン</dt>
+  <dd id="last-logined-at">#{last_login[:created_at]}</dd>
+  <dt>最終ログインIPアドレス</dt>
+  <dd id="last-logined-ip">#{last_login[:ip]}</dd>
+</dl>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    お客様ご契約ID：#{last_login[:login]} 様の代表口座
+  </div>
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-sm-4">
+        普通預金<br>
+        <small>東京支店　1111111111</small><br>
+      </div>
+      <div class="col-sm-4">
+        <p id="zandaka" class="text-right">
+          ―――円
+        </p>
+      </div>
+
+      <div class="col-sm-4">
+        <p>
+          <a class="btn btn-success btn-block">入出金明細を表示</a>
+          <a class="btn btn-default btn-block">振込・振替はこちらから</a>
+        </p>
+      </div>
+
+      <div class="col-sm-12">
+        <a class="btn btn-link btn-block">定期預金・住宅ローンのお申込みはこちら</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+
+  </body>
+</html>
+      HTML
     end
 
     # Validated necessary parameters
