49a3528c104b90dd06b18a2b6eed51cb3da34eb4
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-04 12:35:56 +0900

    Revert "Use users from hash"
    This reverts commit b9037862a763dd8890da0d66896a840c43a088cf.

--- sql/gen.rb
+++ sql/gen.rb
@@ -1,12 +1,10 @@
+source = ""
+
 text = File.read("./dummy_users.tsv")
-
-File.open("users.rb", "w") do |file|
-  file << "USER_BY_LOGIN = {\n"
-
-  text.each_line do |line|
-    id, login, pass, salt, password_hash = line.strip.split("\t")
-    file << "  :'#{login}' => { id: #{id}, salt: '#{salt}', password_hash: '#{password_hash}' },\n"
-  end
-
-  file << "}"
+text.each_line do |line|
+  id, login, pass, salt, password_hash = line.strip.split("\t")
+  source += ":'#{login}' => { id: #{id}, salt: '#{salt}', password_hash: '#{password_hash}' }"
+  print "#{id}         \r"
 end
+
+File.write("src.rb", source)
