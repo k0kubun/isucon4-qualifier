4b2cf8ebf35c45e6aa035ec4daeab81cedaebeea
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-04 23:40:05 +0900

    Revert "On memory users"
    This reverts commit e80cb952269f46c85a1b3758c38909ca7f3189cb.

--- go/db.go
+++ go/db.go
@@ -81,6 +81,7 @@ func isBannedIP(ip string) (bool, error) {
 
 func attemptLogin(req *http.Request) (*User, error) {
 	succeeded := false
+	user := &User{}
 
 	loginName := req.PostFormValue("login")
 	password := req.PostFormValue("password")
@@ -90,17 +91,15 @@ func attemptLogin(req *http.Request) (*User, error) {
 		remoteAddr = xForwardedFor
 	}
 
-	user, err := findUserByLogin(loginName)
-
 	defer func() {
 		createLoginLog(succeeded, remoteAddr, loginName, user)
 	}()
 
-	// row := db.QueryRow(
-	// 	"SELECT id, login, password_hash, salt FROM users WHERE login = ?",
-	// 	loginName,
-	// )
-	// err := row.Scan(&user.ID, &user.Login, &user.PasswordHash, &user.Salt)
+	row := db.QueryRow(
+		"SELECT id, login, password_hash, salt FROM users WHERE login = ?",
+		loginName,
+	)
+	err := row.Scan(&user.ID, &user.Login, &user.PasswordHash, &user.Salt)
 
 	switch {
 	case err == sql.ErrNoRows:
