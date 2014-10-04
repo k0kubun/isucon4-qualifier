65c8ea71992dfd04587cc84489aaf9273d0dd35b
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-04 12:44:48 +0900

    Load user by hash

--- ruby/app.rb
+++ ruby/app.rb
@@ -5,6 +5,7 @@ require 'rack-flash'
 require 'json'
 require "rack-lineprof"
 require "redis"
+require_relative "./users"
 
 module Isucon4
   class App < Sinatra::Base
@@ -80,7 +81,8 @@ module Isucon4
       end
 
       def attempt_login(login, password)
-        user = db.xquery('SELECT * FROM users WHERE login = ?', login).first # 0.8ms
+        # user = db.xquery('SELECT * FROM users WHERE login = ?', login).first # 0.8ms
+        user = USER_BY_LOGIN[login]
 
         if ip_banned? # 0.5ms
           # This will be validated in /report
