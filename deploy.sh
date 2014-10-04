267f56657d95a68f7c2d032e28612e4d50bbc6f3
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-04 21:55:10 +0900

    Faster deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,15 @@ ssh_host=isucon
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-rsync -av -e ssh "${cwd}/" "${ssh_host}:/home/isucon/webapp"
-# ssh $ssh_host "cd /home/isucon/webapp/ruby; /home/isucon/env.sh bundle install"
-ssh $ssh_host "cd /home/isucon/webapp/go; /home/isucon/env.sh /home/isucon/webapp/go/build.sh"
-ssh -t $ssh_host "sudo sysctl -p"
-ssh -t $ssh_host "sudo service mysqld restart"
-ssh -t $ssh_host "sudo service nginx restart"
-# ssh -t $ssh_host "sudo service redis restart"
-# ssh -t $ssh_host "sudo supervisorctl restart isucon_ruby"
-ssh -t $ssh_host "sudo supervisorctl stop isucon_go; sudo rm /tmp/app.sock; sudo supervisorctl start isucon_go; sudo chown nginx:nginx /tmp/app.sock"
+ssh -t -t $ssh_host <<-EOS
+  cd /home/isucon/webapp/go
+  /home/isucon/env.sh /home/isucon/webapp/go/build.sh
+  sudo sysctl -p
+  sudo service mysqld restart
+  sudo service nginx restart
+  sudo service supervisord reload
+  sudo supervisorctl stop isucon_go
+  sudo rm -f /tmp/app.sock
+  sudo supervisorctl start isucon_go
+  exit
+EOS
