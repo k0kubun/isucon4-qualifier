7cd8b68bf78df9f529bd64b25f16d6b5f27a2cbe
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-05 15:33:51 +0900

    Disable serving static assets by martini

--- go/main.go
+++ go/main.go
@@ -101,7 +101,7 @@ func main() {
 	store := sessions.NewCookieStore([]byte("secret-isucon"))
 	m.Use(sessions.Sessions("isucon_go_session", store))
 
-	m.Use(martini.Static("../public"))
+	// m.Use(martini.Static("../public"))
 	m.Use(render.Renderer(render.Options{
 		Layout: "layout",
 	}))
